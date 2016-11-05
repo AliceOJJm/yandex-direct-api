3702c082fed2d682a2c47264d49c2811092dc16d
Elizaveta <aliceo.jjm@gmail.com>
2016-11-05 18:04:48 +0300

    Edit Client class

--- lib/client.rb
+++ lib/client.rb
@@ -4,23 +4,21 @@ require 'json'
 require 'yaml'
 require 'uri'
 
-module YandexDirect
-  class Client
-    attr_reader :ad_groups, :ads, :bids, :campaigns, :dictionaries, :keywords, :sitelinks, :vcards
+class YandexDirect::Client
+  attr_reader :ad_groups, :ads, :bids, :campaigns, :dictionaries, :keywords, :sitelinks, :vcards
 
-    def initialize(@configuration)
-      ad_groups = AdGroup.new(self)
-      ads = Add.new(self)
-      bids = Bid.new(self)
-      campaigns = Campaign.new(self)
-      dictionaries = Dictionarie.new(self)
-      keywords = Keyword.new(self)
-      sitelinks = Sitelink.new(self)
-      vcards = VCard.new(self)
-    end
+  def initialize(@configuration)
+    ad_groups = AdGroup.new(self)
+    ads = Add.new(self)
+    bids = Bid.new(self)
+    campaigns = Campaign.new(self)
+    dictionaries = Dictionarie.new(self)
+    keywords = Keyword.new(self)
+    sitelinks = Sitelink.new(self)
+    vcards = VCard.new(self)
+  end
 
-    def request *args
-      YandexDirect.request(args, config: @configuration)
-    end
+  def request *args
+    YandexDirect.request(args, config: @configuration)
   end
 end
